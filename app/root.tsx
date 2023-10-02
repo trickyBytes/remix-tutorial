651d974a1f56043051e6686734458cf2e1989703
trickyBytes <richard.tiffin@ovo.com>
2023-10-02 09:11:55 +0200

    Create empty contact from new button

--- app/root.tsx
+++ app/root.tsx
@@ -13,11 +13,16 @@ import {
 } from "@remix-run/react";
 
 import appStylesHref from "./app.css";
-import { getContacts } from "./data";
+import { createEmptyContact, getContacts } from "./data";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
 ];
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return json({ contact });
+}
 
 export const loader = async () => {
   const contacts = await getContacts();

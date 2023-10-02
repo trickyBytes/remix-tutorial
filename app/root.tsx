463329fc5902f5cbe4097630624c92f07d20cf2b
trickyBytes <richard.tiffin@ovo.com>
2023-10-02 11:06:49 +0200

    Redirect to edit page on create new

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import type { LinksFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import {
   Form,
   Link,
@@ -21,7 +21,7 @@ export const links: LinksFunction = () => [
 
 export const action = async () => {
   const contact = await createEmptyContact();
-  return json({ contact });
+  return redirect(`/contacts/${contact.id}/edit`);
 }
 
 export const loader = async () => {
